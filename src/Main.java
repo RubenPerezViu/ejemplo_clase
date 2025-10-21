3ed627a1d8fb4618897e677de4c4f7e3855f9185
ruben <rubenpeib@gmail.com>
2025-10-21 12:26:41 +0200

    Versión inicial del programa HolaMundoGitHub

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+/**
+ * Programa HolaMundoGitHub
+ * Muestra un mensaje de saludo por pantalla.
+ * Autor: Ruben Perez Ibañez
+ * Fecha: 21-10-2025
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("¡Hola, GitHub! Este es mi primer repositorio en Java.");
+    }
+}
